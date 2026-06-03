4f0a3c4ea1a0c67a6ed3a85abaf70ef151b6538a
architaa <1010achu.anand@gmail.com>
2026-06-03 14:17:30 +0530

    fix: set DNS to ipv4first for Railway compatibility

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg')
+const dns = require('dns')
 require('dotenv').config()
+
+dns.setDefaultResultOrder('ipv4first')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  family: 4
+  ssl: { rejectUnauthorized: false }
 })
 
 module.exports = pool
