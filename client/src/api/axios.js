81935ccb59794ea831dbe003da968ccb40ed9535
architaa <1010achu.anand@gmail.com>
2026-06-03 14:58:07 +0530

    Day 13: point frontend to production backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://travel-bucket-list-96nk.onrender.com/api'
 })
 
 API.interceptors.request.use((config) => {
